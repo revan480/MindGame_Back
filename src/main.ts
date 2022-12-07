cb1f4a3cb8d59f9871922247209a24b57d82a754
Akif <akifmursalov@gmail.com>
2022-12-07 14:45:57 +0400

    docker fixed

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,11 @@ async function bootstrap() {
     .setTitle('The Mind Game API')
     .setVersion('1.0')
     .addTag('API')
+    
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3333);
+  await app.listen(3000);
 }
 bootstrap();
